4a0a164fa7be0daf32d748e378e609e9feedf93e
RequieMaster <sindi.corral@gmail.com>
2017-04-05 11:28:55 +0200

    Hola

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,12 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/test', function (req, res, next) {
+  res.send("Hola!");
+});
+
+router.post('/test', function (req, res, next) {
+  res.send("Peticion: ", req.body);
+});
+
 module.exports = router;
