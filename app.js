280a64cce3e65f3df9922157ac513c06b15de0e2
kannchenn <Kangxin@MacBook-Pro-de-Kangxin.local>
2017-04-06 10:21:21 +0200

    start moongose

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+//require('./models/Goals');
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/sono');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
